34f7f8688d429690b22888f263948c9a79a72f9d
Pabsyyy <christiangalinato1218@gmail.com>
2024-11-04 18:39:00 +0800

    initial commit

---
+++ classSubjects.js
@@ -0,0 +1,9 @@
+let subjectTitle = "Data Structures and Algorithms";
+let classSchedule = "9:30-10:30 in Monday and Friday";
+let classroom = "Computer Laboratory A";
+let classInstructor = "Mr. Vitruvius John Barayuga";
+let studentName = "Christian Alford M. Galinato";
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`)
+
+
